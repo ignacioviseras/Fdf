818e486e0cc88023e133f5cfb427abb6ef77987d
ignacioviseras <ignacio.viseras@gmail.com>
2024-02-18 20:52:07 +0100

    add atoi base

--- params.c
+++ params.c
@@ -6,7 +6,7 @@
 /*   By: igvisera <igvisera@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 15:00:11 by igvisera          #+#    #+#             */
-/*   Updated: 2024/02/18 17:05:07 by igvisera         ###   ########.fr       */
+/*   Updated: 2024/02/18 20:29:43 by igvisera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,27 +22,30 @@ char	*validate_file(char *file_name)
 	len_file = ft_strlen(file_name);
 	len_extension = len_file - 4;
 	if (ft_strcmp(file_name + len_extension, type_extension) == 0 && len_file > 4)
-		ft_printf("es .fdf'\n");
+		return (file_name);
 	else
 	{
 		ft_printf("#########  Error  #########\n");
 		ft_printf("The program need .fdf extension\n");
-	}
-	return ("asd");
-}
-
-int	main(int argc, char **argv)
-{
-
-	if (argc == 2)
-	{
-		// ft_printf("param->'%s'\n", argv[1]);
-		validate_file(argv[1]);
-	}
-	else
-	{
-		ft_printf("#########  Error  #########\n");
-		ft_printf("The program need only 2 params\n");
-		return (0);
+		return (NULL);
 	}
 }
+
+
+/*
+	Program main
+*/
+// int	main(int argc, char **argv)
+// {
+
+// 	if (argc == 2)
+// 	{
+// 		validate_file(argv[1]);
+// 	}
+// 	else
+// 	{
+// 		ft_printf("#########  Error  #########\n");
+// 		ft_printf("The program need only 2 params\n");
+// 		return (0);
+// 	}
+// }
--- parsing.c
+++ parsing.c
@@ -3,70 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: igvisera <igvisera@student.42.fr>          +#+  +:+       +#+        */
+/*   By: igvisera <igvisera@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 17:26:30 by igvisera          #+#    #+#             */
-/*   Updated: 2024/02/17 20:57:18 by igvisera         ###   ########.fr       */
+/*   Updated: 2024/02/18 17:25:21 by igvisera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fdf.h"
+// #include "fdf.h"
 
 
-void	insert_matrix(char *str)
-{
-	// char **aux;
-	// char **matrix;
-	t_map mapa;
-	
-	//aux = matrix;
-	//*matrix = str;
-	//matrix++;
-	//free(aux);
-	printf("insert_matrix\n", str);
-	// printf("insert_matrix\n%s", *aux);
-}
-
-void	extract_content()
-{
-	char file[] = "./maps/42.fdf";
-	int fd;
-	char *str;
-
-	fd = open(file, O_RDONLY);
-	str = get_next_line(fd);
-	while (str != NULL)
-	{
-		if (str == NULL)
-			ft_printf("Error al abrir el archivo.\n");
-		else
-		{
-			// printf("%s", str);
-			insert_matrix(str);
-			free(str);
-		}
-		str = get_next_line(fd);
-	}
-	free(str);
-	close(fd);
-}
-
-
-
-// void	parsing()
+// void	insert_matrix(char *str)
 // {
-// 	extract_content
+// 	// char **aux;
+// 	// char **matrix;
+// 	t_map mapa;
+	
+// 	//aux = matrix;
+// 	//*matrix = str;
+// 	//matrix++;
+// 	//free(aux);
+// 	printf("insert_matrix\n", str);
+// 	// printf("insert_matrix\n%s", *aux);
 // }
 
-void leaks() {
-	system("leaks fdf");
-}
+// void	extract_content()
+// {
+// 	char file[] = "./maps/42.fdf";
+// 	int fd;
+// 	char *str;
 
-int	main()
-{
-	atexit(leaks);
-	extract_content();
-}
+// 	fd = open(file, O_RDONLY);
+// 	str = get_next_line(fd);
+// 	while (str != NULL)
+// 	{
+// 		if (str == NULL)
+// 			ft_printf("Error al abrir el archivo.\n");
+// 		else
+// 		{
+// 			// printf("%s", str);
+// 			insert_matrix(str);
+// 			free(str);
+// 		}
+// 		str = get_next_line(fd);
+// 	}
+// 	free(str);
+// 	close(fd);
+// }
+
+
+
+// // void	parsing()
+// // {
+// // 	extract_content
+// // }
+
+// void leaks() {
+// 	system("leaks fdf");
+// }
+
+// int	main()
+// {
+// 	atexit(leaks);
+// 	extract_content();
+// }
 
 // int	main (void)
 // {
