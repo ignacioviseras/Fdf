596f50c89e7c52158c22266d0f88d89fb612b7b5
iviseras <ignacio.viseras@gmail.com>
2024-03-07 21:37:26 +0100

    start to draw matrix (don't found)

--- draw_map.c
+++ draw_map.c
@@ -6,7 +6,7 @@
 /*   By: igvisera <igvisera@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 18:16:28 by igvisera          #+#    #+#             */
-/*   Updated: 2024/03/07 19:08:54 by igvisera         ###   ########.fr       */
+/*   Updated: 2024/03/07 21:25:42 by igvisera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,16 +105,103 @@
 // }
 
 // ################          horizontal           ######################
-int main()
-{
-    void *mlx;
-    void *window;
+// int main()
+// {
+//     void *mlx;
+//     void *window;
 
-    mlx = mlx_init();
-    window = mlx_new_window(mlx, WIDTH_WIN, HEIGHT_WIN, "Fdf");
-    for (int i=0; i<100; ++i)
+//     mlx = mlx_init();
+//     window = mlx_new_window(mlx, WIDTH_WIN, HEIGHT_WIN, "Fdf");
+//     for (int i=0; i<100; ++i)
+//     {
+//         mlx_pixel_put(mlx, window, 250 + i, 250, 0xff0000);
+//     }
+//     mlx_loop(mlx);
+// }
+
+
+// ################          cosa rara           ######################
+// int main()
+// {
+//     void *mlx;
+//     void *window;
+//     int size_x;
+//     int size_y;
+
+//     size_x = 0;
+//     mlx = mlx_init();
+//     window = mlx_new_window(mlx, WIDTH_WIN, HEIGHT_WIN, "Fdf");
+//     // for (int i = 0; i < 100; ++i)
+//     // {
+//     //     mlx_pixel_put(mlx, window, 250 + i, 250, 0xff0000);
+//     // }
+//     for ( int y = HEIGHT_WIN * 0.1; y < HEIGHT_WIN * 0.9; ++y)
+//     {
+//         for ( int x = WIDTH_WIN * 0.1; x < WIDTH_WIN * 0.9; ++x)
+//         {
+//             mlx_pixel_put(mlx, window, 250 + x, size_y, 0xff0000);
+//             size_x = x;
+//         }
+//         mlx_pixel_put(mlx, window, size_x, 250 + y, 0xff0000);
+//         size_y = y;
+//     }
+//     mlx_loop(mlx);
+// }
+
+
+
+
+//##############################
+void pixel_put(t_img *img, int x, int y, int color)
+{
+    int offset;
+
+    offset = ((img->line_length * y) + (x * (img->bits_per_pixel /7)))/5;
+    *((unsigned int *)(offset + img->img_pixel_ptr)) = color;
+}
+
+void    screen_pixels(t_window *window, int color)
+{
+    for ( int y = HEIGHT_WIN * 0.1; y < HEIGHT_WIN * 0.9; ++y)
     {
-        mlx_pixel_put(mlx, window, 250 + i, 250, 0xff0000);
+        for ( int x = WIDTH_WIN * 0.1; x < WIDTH_WIN * 0.9; ++x)
+        {
+            pixel_put(&window->img, x, y, color);
+        }
     }
-    mlx_loop(mlx);
+}
+
+int f(int keysym, t_window *window)
+{
+    printf("tecla pulsada %i\n", keysym);
+    //ver q han pulsado y sacar color
+    if (keysym == N_1)//    inverido
+        screen_pixels(window, 0x00ff7f);
+    else if (keysym == N_2)//    rosalige
+        screen_pixels(window, 0xec53a0);
+    else if (keysym == N_3)//    fuchsia
+        screen_pixels(window, 0xff00ff);
+    else if (keysym == ESC)//    salimos
+        exit(1);
+    mlx_put_image_to_window(window->mlx, window->win, window->img.img_ptr, 0, 0);
+    return (0);
+}
+
+int main(void)
+{
+    t_window window;
+
+    window.mlx = mlx_init();
+    window.win = mlx_new_window(window.mlx, WIDTH_WIN, HEIGHT_WIN, "Fdf");
+    
+    window.img.img_ptr = mlx_new_image(window.mlx, WIDTH_WIN, HEIGHT_WIN);
+	window.img.img_pixel_ptr = mlx_get_data_addr(
+                                window.img.img_ptr , 
+                                &window.img.bits_per_pixel, 
+                                &window.img.line_length,
+								&window.img.endian);
+    mlx_key_hook(window.win, f, &window);
+    mlx_loop(window.mlx);
+    return (0);
+    // return (free_all((void **)map), 0);
 }
